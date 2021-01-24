a435c37c568f286f49ba9fe6b03509ab2dd5f50f
Djibril Ba <djibba16084@gmail.com>
2021-01-24 12:54:37 +0000

    Création du projet et ajout du formulaire de création d'article

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home", methods="GET")
+     */
+    public function index(ArticleRepository $repo): Response
+    {
+        $article = $repo->findBy([], ['createdAt' => 'DESC']);
+
+        return $this->render('article/index.html.twig', [
+            'articles' => $article
+        ]);
+    }
+
+    /**
+     * @Route("/article/new", name="app_article_create", methods={"GET", "POST"} )
+     */
+    public function create(Request $request, EntityManagerInterface $manager): Response
+    {
+        $article = new Article();
+
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) { 
+            
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+        
+        return $this->render('article/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/article/{id<[0-9]+>}", name="app_article_show", methods={"GET"})
+     */
+    public function show(Article $article)
+    {
+        return $this->render('article/show.html.twig', [
+            'article' => $article
+        ]);
+    }
+
+    /**
+     * @Route("/article/{id<[0-9]+>}/edit",name="app_article_edit", methods={"GET", "PUT"})
+     */
+    public function edit(Article $article,Request $request, EntityManagerInterface $manager)
+    {
+
+        $form = $this->createForm(ArticleType::class, $article, [
+            'method' => 'PUT'
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) { 
+            
+            $manager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('article/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/article/{id<[0-9]+>}/delete",name="app_article_delete", methods="DELETE")
+     */
+    public function delete(Article $article, Request $request, EntityManagerInterface $manager)
+    {
+        if ($this->isCsrfTokenValid('article_deletion_' . $article->getId(), $request->request->get('csrf_token'))) {
+
+            $manager->remove($article);
+            $manager->flush();    
+            
+        }
+        
+        
+        return $this->redirectToRoute('app_home');
+    }
+
+}
---
+++ src/Entity/Article.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ * @ORM\HasLifecycleCallbacks
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
+     */
+    private $updatedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function updateTimestamps()
+    {
+        if ($this->getCreatedAt() == null) {
+            $this->setCreatedAt(new \DateTimeImmutable);
+        }
+        $this->setUpdatedAt(new \DateTimeImmutable);
+    }
+}
