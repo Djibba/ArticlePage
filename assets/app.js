c4657e8aff49aef98187b5d15f0aeabd50f4e404
Djibril Ba <djibba16084@gmail.com>
2021-01-25 15:04:17 +0000

    Ajustement des image et recuperation du nom de l'image

--- assets/app.js
+++ assets/app.js
@@ -12,3 +12,8 @@ import $ from 'jquery'
 
 // start the Stimulus application
 import './bootstrap';
+
+$('.custom-file-input').on('change', function(e) {
+    var inputFile = e.currentTarget;
+    $(inputFile).parent().find('.custom-file-label').html(inputFile.files[0].name);
+});
