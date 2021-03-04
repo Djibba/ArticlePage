2059eebbef08c758c076155e09a5bc0483f10e1d
Djibril Ba <djibba16084@gmail.com>
2021-03-04 02:19:51 +0000

    Ajout des fonctionnalités de reinitialisation de mot de passe et ajout de maildv

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Fidry\PsyshBundle\PsyshBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -57,7 +57,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('djibba2016@gmail.com', 'Djiby Ba'))
+                    ->from(new Address('djibba@gmail.com', 'Djiby'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
